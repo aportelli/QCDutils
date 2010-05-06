d2dc7dee5f2bf4ff5cfd9330af0377d49fe13d54
Antonin Portelli <antonin.portelli@me.com>
2010-05-06 16:55:54 +0200

    more flexible arg parser

--- qcd_arg_parse.c
+++ qcd_arg_parse.c
@@ -1,63 +1,94 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include "config.h"
 #include "qcd_arg_parse.h"
 #include <argtable2.h>
+#include <latan/latan_nunits.h>
 
 #define A_MAX_NERROR 20
+#define A_TABLE_DEF_SIZE 8
 
-qcd_options qcd_arg_parse(int argc, char* argv[])
+qcd_options qcd_arg_parse(int argc, char* argv[], int argset_flag)
 {
 	/*			argument definitions			*/
 	/********************************************/
+	void** a_table;
+	size_t a_table_size,i;
 	struct arg_lit*	a_help;
 	struct arg_lit* a_ver;
 	struct arg_lit* a_save;
 	struct arg_lit* a_noplot;
+	struct arg_dbl* a_latspac_fm;
+	struct arg_str* a_qcomp;
 	struct arg_str* a_spec;
 	struct arg_str* a_part;
 	struct arg_int* a_source;
 	struct arg_int* a_sink;
 	struct arg_file* a_manf;
 	struct arg_end* a_end;
-	stringbuf help_msg,ver_msg,save_msg,noplot_msg,spec_msg,part_msg,\
-	source_msg,sink_msg,manf_msg;
+	stringbuf help_msg,ver_msg,save_msg,noplot_msg,latspac_fm_msg,qcomp_msg,\
+	spec_msg,part_msg,source_msg,sink_msg,manf_msg;
 	
-	sprintf(help_msg	,"display this help"             		);
-	sprintf(ver_msg		,"display QCDutils version"         	);
-	sprintf(save_msg	,"save resampled samples"				);
-	sprintf(noplot_msg	,"disable plot"							);
-	sprintf(spec_msg	,"particle spectrum (default: qcd)"		);
-	sprintf(part_msg	,"particle name"	             		);
-	sprintf(source_msg	,"particle source"	             		);
-	sprintf(sink_msg	,"particle sink"	            		);
-	sprintf(manf_msg	,"particle LatAnalyze data manifest"	);
+	sprintf(help_msg		,"display this help"             		);
+	sprintf(ver_msg			,"display QCDutils version"         	);
+	sprintf(save_msg		,"save resampled samples"				);
+	sprintf(noplot_msg		,"disable plot"							);
+	sprintf(latspac_fm_msg	,"lattice spacing (in fm)"				);
+	sprintf(qcomp_msg		,"quark composition"					);
+	sprintf(spec_msg		,"particle spectrum (default: qcd)"		);
+	sprintf(part_msg		,"particle name"	             		);
+	sprintf(source_msg		,"particle source"	             		);
+	sprintf(sink_msg		,"particle sink"	            		);
+	sprintf(manf_msg		,"particle LatAnalyze data manifest"	);
 	
-	a_help  	= arg_lit0(NULL,"help",help_msg);
-	a_ver   	= arg_lit0(NULL,"version",ver_msg);
-	a_save		= arg_lit0(NULL,"save",save_msg);
-	a_noplot	= arg_lit0(NULL,"no-plot",noplot_msg);
-	a_spec  	= arg_str0("s","spectrum","{qcd|qcdqed}",spec_msg);
-	a_part  	= arg_str1("p","particle","NAME",part_msg);
-	a_source	= arg_int1("A","source",NULL,source_msg);
-	a_sink  	= arg_int1("B","sink",NULL,sink_msg);
-	a_manf		= arg_file1(NULL,NULL,"<manifest file>",manf_msg);
-	a_end		= arg_end(A_MAX_NERROR);
+	a_help			= arg_lit0(NULL,"help",help_msg);
+	a_ver			= arg_lit0(NULL,"version",ver_msg);
+	a_save			= arg_lit0(NULL,"save",save_msg);
+	a_noplot		= arg_lit0(NULL,"noplot",noplot_msg);
+	a_latspac_fm	= arg_dbl0("L","latspac",NULL,latspac_fm_msg);
+	a_qcomp			= arg_str1(NULL,"qcomp","q1q2",qcomp_msg);
+	a_spec			= arg_str0("s","spectrum","{qcd|qcdqed}",spec_msg);
+	a_part			= arg_str1("p","particle","NAME",part_msg);
+	a_source		= arg_int1("A","source",NULL,source_msg);
+	a_sink			= arg_int1("B","sink",NULL,sink_msg);
+	a_manf			= arg_file1(NULL,NULL,"<manifest file>",manf_msg);
+	a_end			= arg_end(A_MAX_NERROR);
 	
-	void* a_table[] = 
+	a_table_size = A_TABLE_DEF_SIZE;
+	MALLOC_ERRVAL(a_table,void**,a_table_size,NULL);
+	a_table[0] = a_help;
+	a_table[1] = a_ver;
+	a_table[2] = a_save;
+	a_table[3] = a_noplot;
+	i = 4;
+	if (argset_flag & A_LATSPAC)
 	{
-		a_help, 	\
-		a_ver,  	\
-		a_save,		\
-		a_noplot,	\
-		a_spec, 	\
-		a_part, 	\
-		a_source,	\
-		a_sink,		\
-		a_manf,		\
-		a_end		\
-	};
+		a_table_size++;
+		REALLOC_ERRVAL(a_table,a_table,void**,a_table_size,NULL);
+		a_table[i] = a_latspac_fm;
+		i++;
+	}
+	if (argset_flag & A_QCOMP)
+	{
+		a_table_size++;
+		REALLOC_ERRVAL(a_table,a_table,void**,a_table_size,NULL);
+		a_table[i] = a_qcomp;
+		i++;
+	}
+	if (argset_flag & A_PARTICLE)
+	{
+		a_table_size += 2;
+		REALLOC_ERRVAL(a_table,a_table,void**,a_table_size,NULL);
+		a_table[i] = a_spec;
+		a_table[i+1] = a_part;
+		i += 2;
+	}
+	a_table[i] = a_source;
+	a_table[i+1] = a_sink;
+	a_table[i+2] = a_manf;
+	a_table[i+3] = a_end;
 	
 	/*			check arg table allocation		*/
 	/********************************************/
@@ -100,15 +131,39 @@ qcd_options qcd_arg_parse(int argc, char* argv[])
 	}
 	opt->do_save_rs_sample = (a_save->count > 0);
 	opt->do_plot = !(a_noplot->count > 0);
-	strcpy(opt->spec_name,a_spec->sval[0]);
-	strcpy(opt->part_name,a_part->sval[0]);
+	if (argset_flag & A_LATSPAC)
+	{
+		if (a_latspac_fm->count == 1)
+		{
+			opt->latspac_fm = a_latspac_fm->dval[0];
+			opt->latspac_nu = opt->latspac_fm*NU_FM;
+			opt->have_latspac = true;
+		}
+		else
+		{
+			opt->latspac_fm = 1.0;
+			opt->latspac_nu = 1.0;
+			opt->have_latspac = false;
+			fprintf(stderr,"warning: no lattice spacing specified, results will be in lattice unit\n");
+		}
+	}
+	if (argset_flag & A_QCOMP)
+	{
+		strcpy(opt->qcomp,a_qcomp->sval[0]);
+	}
+	if (argset_flag & A_PARTICLE)
+	{
+		strcpy(opt->spec_name,a_spec->sval[0]);
+		strcpy(opt->part_name,a_part->sval[0]);
+	}
 	opt->source = a_source->ival[0];
 	opt->sink = a_sink->ival[0];
 	strcpy(opt->manf_name,a_manf->filename[0]);
 	
 	/*				desallocation				*/
 	/********************************************/
-	arg_freetable(a_table,sizeof(a_table)/sizeof(a_table[0]));
+	arg_freetable(a_table,a_table_size);
+	FREE(a_table);
 
 	return opt;
 }
--- qcd_arg_parse.h
+++ qcd_arg_parse.h
@@ -1,10 +1,24 @@
 #ifndef QCD_ARG_PARSE_H_
 #define QCD_ARG_PARSE_H_
 
-#include <latan/spectrum.h>
+#include <latan/latan_hadron.h>
+
+enum
+{
+	A_DEFAULT	= 0x0,
+	A_LATSPAC	= 0x1,
+	A_PARTICLE	= 0x2,
+	A_QCOMP		= 0x4,
+	A_PROP_FIT	= 0x8,
+	A_CHI_FIT	= 0x10
+};
 
 typedef struct
 {
+	double latspac_fm;
+	double latspac_nu;
+	bool have_latspac;
+	stringbuf qcomp;
 	stringbuf spec_name;
 	stringbuf part_name;
 	stringbuf manf_name;
@@ -16,6 +30,6 @@ typedef struct
 	bool do_save_rs_sample;
 }* qcd_options;
 
-qcd_options qcd_arg_parse(int argc, char* argv[]);
+qcd_options qcd_arg_parse(int argc, char* argv[], int argset_flag);
 
 #endif
