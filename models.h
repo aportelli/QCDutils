75e6314e2265ac4c70619dd2035f61a76425d725
Antonin Portelli <antonin.portelli@me.com>
2012-02-01 15:55:44 +0100

    extern access of model functions

--- models.h
+++ models.h
@@ -7,6 +7,18 @@
 #define DMSQ_K (SQ(NU_M_K_p)-SQ(NU_M_K_0))
 
 /* prototypes */
+double fm_phypt_a_taylor_func(const mat *X, const mat *p, void *vparam);
+size_t fm_phypt_a_taylor_npar(void* vparam);
+void   fm_phypt_a_taylor_pstr(strbuf str, const size_t i,   \
+                              const mat *x_ex, const mat *p,\
+                              void *vparam);
+double fm_scaleset_taylor_func(const mat *X, const mat *p, void *vparam);
+size_t fm_scaleset_taylor_npar(void* vparam);
+void   fm_scaleset_taylor_pstr(strbuf str, const size_t i,   \
+                               const mat *x_ex, const mat *p,\
+                               void *vparam);
+size_t fm_comb_phyt_scale_npar(void* vparam);
+
 extern const fit_model fm_phypt_a_taylor;
 extern const fit_model fm_scaleset_taylor;
 extern const fit_model fm_comb_phyt_scale_taylor;
