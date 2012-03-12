c274613ca3f1fc58781fa15cb5677da5588e5051
Antonin Portelli <antonin.portelli@me.com>
2012-03-12 10:54:38 +0100

    size fix

--- prop.c
+++ prop.c
@@ -87,7 +87,7 @@ int main(int argc, char* argv[])
     rs_sample_set_name(s_mprop,sample_name);
     qcd_printf(opt,"-- resampling %s mean propagator...\n",h->name);
     randgen_set_state(opt->state);
-    resample(s_mprop,prop,ndat,&rs_mean,BOOT,NULL);
+    resample(s_mprop,prop,nbdat,&rs_mean,BOOT,NULL);
     mprop = rs_sample_pt_cent_val(s_mprop);
     
     /*  computing error on mean propagator      */
