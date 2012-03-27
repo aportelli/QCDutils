9c800bf52880bf4b79915b28f33812536a1987af
Antonin Portelli <antonin.portelli@me.com>
2012-03-27 15:48:07 +0200

    example of Mathematica notebook to generate tabulated function code

---
+++ qed_fvol_tabfunc.h
@@ -0,0 +1,12 @@
+#ifndef QED_FVOL_TABFUNC_H_
+#define QED_FVOL_TABFUNC_H_
+
+#include <latan/latan_tabfunc.h>
+
+#define QED_FVOL_KAPPA 2.83729747947876
+
+extern tabfunc qed_fvol_M;
+extern tabfunc qed_fvol_H;
+extern tabfunc qed_fvol_K;
+
+#endif
